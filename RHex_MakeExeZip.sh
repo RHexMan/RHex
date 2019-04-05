df15195558e595adf3e208c7bca202bddbc61d10
“RHexMan” <rich@ski.org>
2019-04-05 13:56:28 -0700

    removed automatic files in RUtils

---
+++ RHex_MakeExeZip.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# RHex_MakeExeZip
+
+exe_name=$0
+exe_dir=`dirname $0`
+cd $exe_dir
+echo "exe_dir=" $exe_dir
+echo "------------------------------------------"
+echo
+
+mkdir RHex_Exe
+
+pp -o rhexexe -c RHexSwing3D.pl RHexReplot3D.pl
+
+mv rhexexe RHex_Exe
+
+cp rgnuplot rgnuplotx RHex_Exe
+
+cp RHex_INSTALL.sh RHex_Exe
+RHex_Exe/RHex_INSTALL.sh
+
+cp -R SpecFiles_Leader SpecFiles_Line SpecFiles_Preference SpecFiles_SwingDriver RHex_Exe/
+
+cp Documents/README.md Documents/ParameterList_Swing.md RHex_Exe
+
+exit
